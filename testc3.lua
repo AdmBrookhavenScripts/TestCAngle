dd228e5296ca7d34b33506d7d1ad5616bb61b1ec
AdmBrookhavenScripts <168764106+AdmBrookhavenScripts@users.noreply.github.com>
2026-04-08 19:49:58 -0300

    Create testc3.lua

---
+++ testc3.lua
@@ -0,0 +1,48 @@
+--[[
+  Licensed under the MIT License (see LICENSE file for full details).
+  Copyright (c) 2026 MrY7zz
+
+  LEGAL NOTICE:
+  You are REQUIRED to retain this license header under the terms of the MIT License.
+  Removing or modifying this notice may violate copyright law.
+]]
+--// BY MrY7zz
+
+return (function(...)
+	local args = { ... }
+	local fakeCharacter = nil
+	local realChar = args[1] or game:GetService("Players").LocalPlayer.Character or game:GetService("Players").LocalPlayer.CharacterAdded:Wait()
+	local willClone = args[2]
+	local rigType = args[3]
+	local realCharCFrame = realChar.HumanoidRootPart.CFrame
+	
+	local Players = game:GetService("Players")
+	local camera = workspace.CurrentCamera
+	
+	if willClone then
+		local value = realChar.Archivable
+		realChar.Archivable = true
+		fakeCharacter = realChar:Clone()
+		realChar.Archivable = value
+		fakeCharacter.HumanoidRootPart.CFrame = realCharCFrame
+		
+		fakeCharacter:SetAttribute("IsFake", true)
+		
+		camera.CameraSubject = fakeCharacter:FindFirstChildOfClass("Humanoid")
+		
+		return fakeCharacter
+	else
+		fakeCharacter = Players:CreateHumanoidModelFromDescription(
+			realChar.Humanoid:FindFirstChild("HumanoidDescription") or Instance.new("HumanoidDescription"),
+			rigType
+		)
+		
+		fakeCharacter.HumanoidRootPart.CFrame = realCharCFrame
+		
+		fakeCharacter:SetAttribute("IsFake", true)
+		
+		camera.CameraSubject = fakeCharacter:FindFirstChildOfClass("Humanoid")
+		
+		return fakeCharacter
+	end
+end)(...)
